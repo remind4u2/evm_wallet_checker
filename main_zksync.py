e5f8def36d2c06ad44c6d7578e3923dc67068af7
bizire <aa952393@gmail.com>
2024-02-29 18:16:49 +0300

    zk

--- main_zksync.py
+++ main_zksync.py
@@ -1,10 +1,18 @@
 import time
+from web3 import Web3
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-TIMEOUT = 8 # подбираем для себя комфортную задержку. На быстрых машинах достаточно 1 сек, и ногда надо и 10 сек
+TIMEOUT = 3 # подбираем для себя комфортную задержку. На быстрых машинах достаточно 1 сек, и ногда надо и 10 сек
+rpcs = [
+    # 'https://1rpc.io/zksync2-era', 
+    'https://mainnet.era.zksync.io', 
+    # 'https://zksync-era.blockpi.network/v1/rpc/public', 
+    # 'https://zksync.meowrpc.com'
+    ]
 
 if __name__ == "__main__":
     with open("wallets.txt", "r") as f:
@@ -20,11 +28,13 @@ if __name__ == "__main__":
         # Initialize the WebDriver with the provided Chrome driver path and options
         driver = webdriver.Chrome(options=options)
         print()
-        print(f'Начинаем проверку кошельков в layerzeroscan:')
+        print(f'Начинаем проверку кошельков в zkflow')
+        print(f'Формат:')
+        print(f'Адрес; количество транз; баланс ETH; объем $; комиссии $; месяцы; недели; дни')
         print()
-        print(f'------------------------------------------------')
-        print(f'| кошелек                              |транз  |')
-        print(f'------------------------------------------------')
+        print(f' ---------------------------------------------------------------------')
+        print(f'| кошелек                                 |тр|баланс|объем|комс|м|н|д|')
+        print(f' ---------------------------------------------------------------------')
         total_tx = 0
         for wallet in WALLETS:
             # Fetch the HTML content of the webpage
@@ -33,34 +43,72 @@ if __name__ == "__main__":
             driver.get(url)
             time.sleep(TIMEOUT) 
 
+            rpc = random.choice(rpcs)
+            web3 = Web3(Web3.HTTPProvider(rpc))
+
+            wallet  = Web3.to_checksum_address(wallet)
+            nonce = web3.eth.get_transaction_count(wallet)
+            balance = web3.eth.get_balance(wallet)
+            balance = round(Web3.from_wei(balance, 'ether'), 5)
+
             # Wait for the element with class "mui-hx4d9l" to appear (timeout after 10 seconds)
             div_elements = WebDriverWait(driver, 10).until(
                 EC.presence_of_all_elements_located((By.XPATH, '//div[@font-size="3rem" and @font-weight="800" and @color="#2363eb" and @lineheight="1" and @mb="8px"]'))
             )
             wallet_info = ""  
             # Extract text from each div
+            i = 1
             for div_element in div_elements:
                 # Extract text from the div
-                if div_element:
-                    text = div_element.text
-                    # Extract the number using regex
-                    import re
-                    number = re.search(r'\d+', text)
-                    if number:
-                        
-                        wallet_info = wallet_info +";"+ number.group()
+                if (i > 1) :
+                    if div_element:
+                        text = div_element.text
+                        # Extract the number using regex
+                        import re
+                        number = re.search(r'\d+', text)
+                        if number:
+                            wallet_info = wallet_info +";"+ number.group()
+                        else:
+                            wallet_info = wallet_info +";0"
                     else:
-                        print("No number found in the div.")
-                else:
-                    print("Div element not found.")
+                        print("Div element not found.")
+                i = i + 1
 
-            print(f"{wallet};{wallet_info}")
-        tx_average = round(total_tx/len(WALLETS))
+            import re
+            # Wait for the element with class "mui-hx4d9l" to appear (timeout after 10 seconds)
+            div_elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//div[@font-weight="600" and @font-size="14px" and @color="text"]')) or EC.presence_of_all_elements_located((By.XPATH, '//div[@font-size="14px" and @font-weight="600" and @color="text"]'))
+            )
+
+            # print(f'div_elements size = {len(div_elements)}')
+
+            if (len(div_elements) == 14):
+                # Extract text from each div
+                day_element = div_elements[9].text
+                days = re.search(r'\d+', day_element).group()
+
+                week_element = div_elements[11].text
+                weeks = re.search(r'\d+', week_element).group()
+
+                month_element = div_elements[13].text
+                months = re.search(r'\d+', month_element).group()
+            
+            if (len(div_elements) == 13):
+                # Extract text from each div
+                day_element = div_elements[8].text
+                days = re.search(r'\d+', day_element).group()
+
+                week_element = div_elements[10].text
+                weeks = re.search(r'\d+', week_element).group()
+
+                month_element = div_elements[12].text
+                months = re.search(r'\d+', month_element).group()
+
+            print(f"{wallet};{nonce};{balance}{wallet_info};{months};{weeks};{days}")
         driver.quit()
-        print(f'------------------------------------------------')
+        print(f' ---------------------------------------------------------------------')
         print()
         print(f'Всего проверено {len(WALLETS)} кошельков')
-        print(f'Среднее кол-во транзакций на кошелек: {tx_average}')
         print()
 
     else:
